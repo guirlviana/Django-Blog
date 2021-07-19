209238cc018ba68221b91ef05804d14cd6e0c901
guirlviana <girlviana@gmail.com>
2021-07-19 15:16:18 -0300

    iniciado ligação entre campos

--- posts/views.py
+++ posts/views.py
@@ -2,11 +2,14 @@ from typing import List
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.edit import UpdateView
-
+from .models import Post
 
 
 class PostIndex(ListView):
-    pass
+    model = Post
+    template_name = 'posts/index.html'
+    paginate_by = 1
+    context_object_name = 'posts'
 
 class PostBusca(PostIndex):
     pass
