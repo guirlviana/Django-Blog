7eaefc39dffa09f7daefd08b5d4be9d40caa473c
guirlviana <girlviana@gmail.com>
2021-07-19 11:51:16 -0300

    criado back-end do blog

---
+++ comentarios/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from posts.models import Post
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+class Comentario(models.Model):
+    nome_comentario = models.CharField(max_length=255, verbose_name='Autor')
+    email_comentario = models.EmailField(max_length=255, verbose_name='Email')
+    comentario = models.TextField(verbose_name='Comentário')
+    post_comentario = models.ForeignKey(Post, on_delete=models.CASCADE, verbose_name='Post')
+    usuario_comentario = models.ForeignKey(User, on_delete=models.DO_NOTHING, verbose_name='Usuário')
+    data_comentario = models.DateTimeField(default=timezone.now, verbose_name='Data')
+    publicado_comentario = models.BooleanField(default=False, verbose_name='Está público?')
+
+    def __str__(self) -> str:
+        return self.nome_comentario
---
+++ posts/views.py
@@ -0,0 +1,18 @@
+from typing import List
+from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.edit import UpdateView
+
+
+
+class PostIndex(ListView):
+    pass
+
+class PostBusca(PostIndex):
+    pass
+
+class PostCategoria(PostIndex):
+    pass
+
+class PostDetalhes(UpdateView):
+    pass
